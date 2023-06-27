20a61f1e17e0ecc29d665a057fdb62dc7d96901d
Travis Simpson <Travis.Simpson@fdmgroup.com>
2023-06-27 17:24:15 +0100

    string argument to handle up to 60 digits

--- main.cpp
+++ main.cpp
@@ -8,28 +8,40 @@ const std::string ones[] = {
 const std::string tens[] = {
     "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 
+const std::string affixes[] = {
+    "", " thousand", " million", " billion", " trillion", " quadrillion", " quintillion", " sextillion", 
+    " septillion", " octillion", " nonillion", " decillion", " undecillion",  " duodecillion", " tredecillion", 
+    " quattuordecillion", " quindecillion", " sexdecillion", " septendecillion", " octodecillion"};
+
 std::string getThreeDigitNumberAsWord(int number)
 {
     std::string threeDigitWord;
     int hundreds = number / 100;
-    if(hundreds > 0){
+    if (hundreds > 0)
+    {
         threeDigitWord += ones[hundreds] + " hundred ";
     }
     int tensUnit = number % 100;
-    if (tensUnit > 0) {
-        if (!threeDigitWord.empty()) {
+    if (tensUnit > 0)
+    {
+        if (!threeDigitWord.empty())
+        {
             threeDigitWord += "and ";
         }
 
-        if (tensUnit < 20) {
+        if (tensUnit < 20)
+        {
             threeDigitWord += ones[tensUnit];
-        } else {
+        }
+        else
+        {
             int tensDigit = tensUnit / 10;
             int onesDigit = tensUnit % 10;
 
             threeDigitWord += tens[tensDigit];
 
-            if (onesDigit > 0) {
+            if (onesDigit > 0)
+            {
                 threeDigitWord += "-";
                 threeDigitWord += ones[onesDigit];
             }
@@ -38,11 +50,8 @@ std::string getThreeDigitNumberAsWord(int number)
     return threeDigitWord;
 }
 
-std::string getWordEquivalent(int number)
+std::string getWordEquivalent(int number) //naive method
 {
-    const std::string affixes[] = {
-    "", " thousand", " million", " billion"};
-
     if (number < 1 || number > 2000000000)
     {
         return "Number out of range";
@@ -50,23 +59,54 @@ std::string getWordEquivalent(int number)
 
     std::string wordEquivalent;
     int i = 0;
-    while(number > 0 && i < 4){
-        //Process three digits at a time and work upwards
+    while (number > 0 && i < 4)
+    {
+        // Process three digits at a time and work upwards
         int threeDigitNumber = number % 1000;
-        if(threeDigitNumber > 0){
+        if (threeDigitNumber > 0)
+        {
             std::string threeDigitNumberAsWord = getThreeDigitNumberAsWord(threeDigitNumber);
             wordEquivalent = " " + threeDigitNumberAsWord + affixes[i] + wordEquivalent;
         }
-        
+
         number /= 1000;
         i++;
     }
     return wordEquivalent;
 }
 
+std::string getWordEquivalent(std::string number) // Overloading
+{
+    std::string wordEquivalent;
+    int n = number.length() - 1; // account for indexes (0,...,n-1)
+    if(n > 59){
+        return "Number out of bounds";
+    }
+    int affixIndex = 0;
+    while (n >= 0)
+    {
+        std::string threeDigitString = "";
+        char ten = (n - 1 >= 0) ? number[n - 1] : ' ';
+        char hundred = (n - 2 >= 0) ? number[n - 2] : ' ';
+        threeDigitString += hundred;
+        threeDigitString += ten;
+        threeDigitString += number[n];
+        int threeDigitNumber = std::stoi(threeDigitString);
+        if (threeDigitNumber > 0)
+        {
+            std::string threeDigitNumberAsWord = getThreeDigitNumberAsWord(threeDigitNumber);
+            wordEquivalent = " " + threeDigitNumberAsWord + affixes[affixIndex] + wordEquivalent;
+        }
+
+        affixIndex++;
+        n -= 3;
+    }
+    return wordEquivalent;
+}
+
 int main()
 {
-    int number;
+    std::string number;
     std::cout << "Enter a number: ";
     std::cin >> number;
 
